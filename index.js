9c530a4be737e515a3a32e1114b44cecf607a6ce
Rizkina Muhammad Syam <rizkina.ms@gmail.com>
2024-05-08 15:41:23 +0700

    exercise

--- index.js
+++ index.js
@@ -2,9 +2,22 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
+//middleware
+const logger = (req,res, next) =>{
+    console.log(`$(req.method) ${req.url}`);
+    next();
+}
+app.use(logger);
+
 app.get('/',(req, res) =>
     res.send('Hello World')
 );
+
+app.use(express.json());
+app.use(express.urlencoded({extended:false}));
+
+//router
+
 
 app.get('/datajson',(req, res) =>
     res.json([{
@@ -15,6 +28,8 @@ app.get('/datajson',(req, res) =>
         'name':'jono'
     }])
 );
+
+
 
 app.listen(port,() => console.log(`working at http://localhost:/${port}`))
 

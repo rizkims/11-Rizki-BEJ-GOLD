be8dd4d3dd17954463042fb4c4d14c6b80f8842b
Rizkina Muhammad Syam <rizkina.ms@gmail.com>
2024-05-08 14:57:51 +0700

    develop app

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-console.log('hello world');
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.get('/',(req, res) =>
+    res.send('Hello World s')
+);
+
+app.listen(port,() => console.log(`working at http://localhost:/${port}`))
+
